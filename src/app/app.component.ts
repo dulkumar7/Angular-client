36f952da8e7b5e0af3e19d52593df0133de73e11
167817 <167817@USDSPLNJ9W3MQ2.AMERICAS.GLOBAL.NTTDATA.COM>
2019-02-01 14:55:57 -0600

    angular-client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'customer-app';
+  title = 'Customer-App';
+  description = 'Demo Customer App'
 }
